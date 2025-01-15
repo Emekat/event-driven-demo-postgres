40de04d1fa408fe239b6fc1d5892a3638ec81064
Chukwuemeka Torti <emekatorti@gmail.com>
2025-01-15 14:43:09 -0500

    feat: added background service

---
+++ api/EventSubscriber.cs
@@ -0,0 +1,14 @@
+namespace api;
+
+public class EventSubscriber : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
