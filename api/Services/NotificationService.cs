96824c765af008677a81471f997a248e528417b5
Chukwuemeka Torti <emekatorti@gmail.com>
2025-01-17 08:23:57 -0500

    refact: primary constructor

--- api/Services/NotificationService.cs
+++ api/Services/NotificationService.cs
@@ -2,17 +2,11 @@ using api.Contracts;
 
 namespace api.Services;
 
-public class NotificationService : INotificationService
+public class NotificationService(ILogger<NotificationService> logger) : INotificationService
 {
-    private readonly ILogger<NotificationService> _logger;
-
-    public NotificationService(ILogger<NotificationService> logger)
-    {
-        _logger = logger;
-    }
     public Task NotifyUser(string title, string message, string? userId = null)
     {
-        _logger.LogInformation("User notified");
+        logger.LogInformation("User notified");
         return Task.CompletedTask;
     }
 }
