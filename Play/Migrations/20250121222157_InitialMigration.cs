10417570d65e2a915842b9e56458bc502b93beef
Chukwuemeka Torti <emekatorti@gmail.com>
2025-01-21 17:39:14 -0500

    migration files

---
+++ Play/Migrations/20250121222157_InitialMigration.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Play.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "events",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    sequence_number = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    aggregate_id = table.Column<string>(type: "text", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    data = table.Column<string>(type: "jsonb", nullable: false),
+                    version = table.Column<int>(type: "integer", nullable: false),
+                    timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_events", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "failed_events",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    sequence_number = table.Column<long>(type: "bigint", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    error_message = table.Column<string>(type: "text", nullable: false),
+                    stack_trace = table.Column<string>(type: "text", nullable: false),
+                    failure_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    retry_count = table.Column<int>(type: "integer", nullable: false),
+                    data = table.Column<string>(type: "jsonb", nullable: false),
+                    timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_processed = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_failed_events", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_events_aggregate_id_version",
+                table: "events",
+                columns: new[] { "aggregate_id", "version" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_events_sequence_number",
+                table: "events",
+                column: "sequence_number",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_failed_events_sequence_number",
+                table: "failed_events",
+                column: "sequence_number",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "events");
+
+            migrationBuilder.DropTable(
+                name: "failed_events");
+        }
+    }
+}
